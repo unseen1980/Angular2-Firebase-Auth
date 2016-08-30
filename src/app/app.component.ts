45da76c42f0a7ddf2c8586afcddd1ee6c6ef09e8
Christos Koutsiaris <koutsiaris@gmail.com>
2016-08-30 15:52:12 +0100

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user = {};
+
+  constructor(public af: AngularFire) {
+    this.af.auth.subscribe(user => {
+      console.log('---->', user)
+      if (user) {
+        this.user = user.auth.providerData[0];
+      }
+      else {
+        this.user = {};
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    });
+  }
+
+  logout = function () {
+    this.af.auth.logout();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import * as firebase from 'firebase';
+
+import { AngularFireModule } from 'angularfire2';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBTjsXaFh4G7UJra80EtTRJtsNjhDVm4A",
+  authDomain: "angular2-links.firebaseapp.com",
+  databaseURL: "https://angular2-links.firebaseio.com",
+  storageBucket: "angular2-links.appspot.com"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
